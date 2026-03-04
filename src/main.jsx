5f0976501e504c9df853a38df795dd1bc09fcb38
Isabel Maito <maitoisabel@gmail.com>
2026-03-03 21:22:45 -0300

    refactor: adicionar classe dark ao body por padrão

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Adiciona classe dark por padrão ao body
+document.body.classList.add('dark');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
